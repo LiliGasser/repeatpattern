8e85995222c6c422ca0f373d7a81c4fd97e01235
Lili Gasser <liliangasser@proton.me>
2025-08-21 17:55:41 +0200

    website with dropdowns, buttons and canvas

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 
-
 // TODO load from file by country
 let gccs_values = {
   'wtp_1': 68,
@@ -9,65 +8,119 @@ let gccs_values = {
   'norm': 91,
   'government': 84,
 }
-let selVar = 'wtp_1';
 
-// TODO buttons for countries
+// dropdown menus for selection and export button
+let selectCountry;
+let selectMotif;
+let selectSymmetry;
+let selectColors;
+let exportButton;
 
 // TODO proper motif with corresponding scales
 let rScale = d3.scaleSqrt();
 let r;
 
+// define postcard size (A5 postcard: 210mm x 148mm)
+// calculation: https://imageonline.co/mm-to-px.php
+// laptop screen: 1920 x 1200, 14'' diagonal --> 162 ppi; 60Hz
+let pcWidthMM = 210; // mm
+let pcHeightMM = 148; // mm
+let dpi = 150; // dots per inch
+let pcWidth = Math.floor(pcWidthMM * dpi / 25.4);
+let pcHeight = Math.floor(pcHeightMM * dpi / 25.4);
+let pcPosX = 300; // x position of postcard
+let pcPosY = 100; // y position of postcard
+
 // define cell
-let cellWidth = 150;
-let cellHeight = 150;
-//let margin = {
-  //top: 0,
-  //right: 0,
-  //bottom: 0,
-  //left: 0
-//}
+let cellWidth = 100;
+let cellHeight = 100;
 let nCells;
 
 function setup() {
-  // TODO how to calculate the size of the canvas in pixels? postcard: 210mm x 148mm
-  // https://imageonline.co/mm-to-px.php with 300 dpi
-  createCanvas(2480, 1748);
+  let cnv = createCanvas(pcWidth, pcHeight);
+  cnv.position(pcPosX, pcPosY);
   noLoop();
 
-  // TODO test for cell sizes where multiplication with width and height does not result in integer values
-  nCells = Math.ceil(width / cellWidth) * Math.ceil(height / cellHeight);
-  console.log('nCells: ' + nCells);
+  // load data from file
+  // TODO continue here!
+  d3.csv("data/gccs_country_with_temperature_and_gdp.csv").then(data => {
+    console.log(data);
+    // TODO filter data by country
 
-  rScale.domain([0, 100]).range([1, 120]);
+  });
+
+
+  // create dropdown menu for country selection
+  // TODO change font size --> tailwindcss
+  selectCountry = createSelect();
+  selectCountry.position(30, 100);
+  selectCountry.option('Switzerland');
+  selectCountry.size(200, 50);
+
+  // create dropdown menu for motif selection
+  selectMotif = createSelect();
+  selectMotif.position(30, 160);
+  selectMotif.option('Motif 1');
+  selectMotif.size(200, 50);
+
+  // create dropdown menu for symmetry selection
+  selectSymmetry = createSelect();
+  selectSymmetry.position(30, 220);
+  selectSymmetry.option('Translation (3.1)');
+  selectSymmetry.size(200, 50);
+
+  // create dropdown menu for color selection
+  selectColors = createSelect();
+  selectColors.position(30, 280);
+  selectColors.option('Color Scheme 1');
+  selectColors.size(200, 50);
+
+  // create export button
+  // TODO export as other file formats (e.g. svg)
+  exportButton = createButton('Export postcard');
+  exportButton.position(30, 340);
+  exportButton.size(200, 50);
+  exportButton.mousePressed(() => {
+    saveCanvas('postcard', 'png');
+  });
+
+  // TODO test for cell sizes where multiplication with width and height does not result in integer values
+  nCells = Math.ceil(pcWidth / cellWidth) * Math.ceil(pcHeight / cellHeight);
+  console.log('nCells: ' + nCells);
+  console.log('pcWidth: ' + pcWidth + ', pcHeight: ' + pcHeight);
+
+
+  rScale.domain([0, 100]).range([1, Math.min(cellWidth, cellHeight) * 0.8]);
 
 }
 
 function draw() {
   background(220);
+  
+  // initialize
   rectMode(TOP, LEFT);
-
-  // initialize x and y
   let x = 0;
   let y = 0;
+
+  // draw pattern
   for (let i = 0; i < nCells; i++) {
 
     // draw grid
     drawGrid(x, y);
 
     // draw motif
+    // TODO draw motif in separate for loop?
     drawMotif(x, y);
 
     // move to next cell: planar symmetry with translation
     // TODO other symmetriy operations
     x = x + cellWidth;
-    if (x >= width) {
+    if (x >= pcWidth) {
       x = 0;
       y += cellHeight;
     }
 
   }
-
-  // TODO draw motif in separate for loop?
 
 }
 
@@ -82,30 +135,26 @@ function drawGrid(x, y) {
 }
 
 function drawMotif(x, y) {
-  console.log('drawMotif at x: ' + x + ', y: ' + y);
+  //console.log('drawMotif at x: ' + x + ', y: ' + y);
   noStroke();
 
   // wtp_1
   r = rScale(gccs_values['wtp_1']);
-  console.log('radius wtp_1: ' + r);
   fill(100, 150, 0);
   arc(x + cellWidth/2, y + cellHeight/2, r, r, -PI/2, 0);
 
   // wtp_1_belief
   r = rScale(gccs_values['wtp_1_belief']);
-  console.log('radius wtp_1_belief: ' + r);
   fill(100, 0, 0);
   arc(x + cellWidth/2, y + cellHeight/2, r, r, 0, PI/2);
 
   // norm
   r = rScale(gccs_values['norm']);
-  console.log('radius norm: ' + r);
   fill(0, 100, 150);
   arc(x + cellWidth/2, y + cellHeight/2, r, r, PI/2, PI);
 
   // government
   r = rScale(gccs_values['government']);
-  console.log('radius government: ' + r);
   fill(100, 0, 200);
   arc(x + cellWidth/2, y + cellHeight/2, r, r, PI, -PI/2);
 
