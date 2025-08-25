4e6ea0f2fef1cdf1a4c8ce9cfe4ee2ee39dffd2a
Lili Gasser <liliangasser@proton.me>
2025-08-25 09:45:29 +0200

    draw postcard again

--- sketch.js
+++ sketch.js
@@ -1,81 +1,131 @@
+// initialize canvas, selects and buttons
 let canvas;
 let countrySelect
 let motifSelect
 let symmetrySelect
 let colorsSelect
 let exportButton;
+
+// initialize data structure
+// TODO might need to be the whole array
+let gccsValues;
+
+// define cell
+let cellWidth = 80;
+let cellHeight = 80;
+let nCells;
+// TODO proper motif with corresponding scales
+let rScale = d3.scaleSqrt();
+let r;
         
 function setup() {
-    canvas = createCanvas(800, 600);
-    canvas.parent(document.querySelector('.canvas-container'));
+  canvas = createCanvas(800, 600);
+  canvas.parent(document.querySelector('.canvas-container'));
             
-    // Get references to HTML elements
-    countrySelect = document.getElementById('country');
-    motifSelect = document.getElementById('motif');
-    symmetrySelect = document.getElementById('symmetry');
-    colorsSelect = document.getElementById('colors');
-    exportButton = document.getElementById('exportButton');
+  // Get references to HTML elements
+  countrySelect = document.getElementById('country');
+  motifSelect = document.getElementById('motif');
+  symmetrySelect = document.getElementById('symmetry');
+  colorsSelect = document.getElementById('colors');
+  exportButton = document.getElementById('exportButton');
             
-    // Set initial background
-    //updateBackground();
+  // Add event listeners
+  exportButton.addEventListener('click', exportCanvas);
             
-    // Add event listeners
-    //backgroundSelect.addEventListener('change', updateBackground);
-    exportButton.addEventListener('click', exportCanvas);
-            
-    // Set initial drawing properties
-    strokeCap(ROUND);
-    strokeJoin(ROUND);
+  // Set initial drawing properties
+  strokeCap(ROUND);
+  strokeJoin(ROUND);
+
+  // Load data
+  d3.dsv(";", "data/gccs_country_with_temperature_and_gdp.csv", d3.autoType).then(function(data) {
+    console.log("Data loaded:", data);
+
+    dataCountry = data.filter(d => d.country === 'Switzerland');
+    console.log("Filtered data for a country:", dataCountry);
+    gccsValues = {
+      temperature: dataCountry[0].temperature,
+      gdp: dataCountry[0].gdp,
+      wtp: dataCountry[0].gccs_wtp,
+      wtp_belief: dataCountry[0].gccs_wtp_belief,
+      norm: dataCountry[0].gccs_norm,
+      government: dataCountry[0].gccs_government,
+    }
+    console.log(gccsValues);
+    
+  });
+  
+  // TODO test for cell sizes where multiplication with width and height does not result in integer values
+  nCells = Math.ceil(width / cellWidth) * Math.ceil(height / cellHeight);
+  console.log('nCells: ' + nCells, 'pcWidth: ' + width + ', pcHeight: ' + height);
+
+  rScale.domain([0, 100]).range([1, Math.min(cellWidth, cellHeight) * 0.8]);
 }
         
 function draw() {
-    if (mouseIsPressed && mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-        //// Get current settings
-        //let brushSize = parseInt(brushSizeSelect.value);
-        //let colorValues = brushColorSelect.value.split(',');
-        //let brushStyle = brushStyleSelect.value;
-                
-        //// Set color
-        //stroke(parseInt(colorValues[0]), parseInt(colorValues[1]), parseInt(colorValues[2]));
-        //strokeWeight(brushSize);
-                
-        //// Apply brush style
-        //if (brushStyle === 'dotted') {
-            //// Draw dots instead of lines
-            //noStroke();
-            //fill(parseInt(colorValues[0]), parseInt(colorValues[1]), parseInt(colorValues[2]));
-            //circle(mouseX, mouseY, brushSize);
-        //} else if (brushStyle === 'dashed') {
-            //// Draw dashed line effect
-            //if (frameCount % 3 === 0) {
-                //line(pmouseX, pmouseY, mouseX, mouseY);
-            //}
-        //} else if (brushStyle === 'rough') {
-            //// Add some randomness for rough effect
-            //for (let i = 0; i < 3; i++) {
-                //let offsetX = random(-brushSize/2, brushSize/2);
-                //let offsetY = random(-brushSize/2, brushSize/2);
-                //point(mouseX + offsetX, mouseY + offsetY);
-            //}
-        //} else {
-            //// Normal line
-            //line(pmouseX, pmouseY, mouseX, mouseY);
-        //}
+  background(220);
+  
+  // initialize
+  rectMode(TOP, LEFT);
+  let x = 0;
+  let y = 0;
+
+  // draw pattern
+  for (let i = 0; i < nCells; i++) {
+
+    // draw grid
+    drawGrid(x, y);
+
+    // draw motif
+    // TODO draw motif in separate for loop?
+    drawMotif(x, y);
+
+    // move to next cell: planar symmetry with translation
+    // TODO other symmetriy operations
+    x = x + cellWidth;
+    if (x >= width) {
+      x = 0;
+      y += cellHeight;
     }
+
+  }
+
 }
-        
-function updateBackground() {
-    let bgValues = backgroundSelect.value.split(',');
-    background(parseInt(bgValues[0]), parseInt(bgValues[1]), parseInt(bgValues[2]));
+
+function drawGrid(x, y) {
+
+  noFill();
+  stroke(100);
+  strokeWeight(1);
+  rect(x, y, cellWidth, cellHeight);
+
 }
-        
+
+function drawMotif(x, y) {
+  //console.log('drawMotif at x: ' + x + ', y: ' + y);
+  noStroke();
+
+  // wtp
+  r = rScale(gccsValues.wtp);
+  fill(100, 150, 0);
+  arc(x + cellWidth/2, y + cellHeight/2, r, r, -PI/2, 0);
+
+  // wtp_belief
+  r = rScale(gccsValues.wtp_belief);
+  fill(100, 0, 0);
+  arc(x + cellWidth/2, y + cellHeight/2, r, r, 0, PI/2);
+
+  // norm
+  r = rScale(gccsValues.norm);
+  fill(0, 100, 150);
+  arc(x + cellWidth/2, y + cellHeight/2, r, r, PI/2, PI);
+
+  // government
+  r = rScale(gccsValues.government);
+  fill(100, 0, 200);
+  arc(x + cellWidth/2, y + cellHeight/2, r, r, PI, -PI/2);
+
+}
+
 function exportCanvas() {
     saveCanvas('my-drawing', 'png');
-}
-        
-// Clear canvas when 'C' key is pressed
-function keyPressed() {
-    if (key === 'c' || key === 'C') {
-        updateBackground();
-    }
 }
