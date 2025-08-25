41c164a741154e378dcf9f0b92bdf5e3b0447f0a
Lili Gasser <liliangasser@proton.me>
2025-08-25 08:38:50 +0200

    index and sketch from claude

--- sketch.js
+++ sketch.js
@@ -1,168 +1,81 @@
-
-// TODO load from file by country
-let gccs_values = {
-  'wtp_1': 68,
-//  'wtp_pos': 75,
-//  'wtp': 71,
-  'wtp_1_belief': 41,
-  'norm': 91,
-  'government': 84,
-}
-
-// dropdown menus for selection and export button
-let selectCountry;
-let selectMotif;
-let selectSymmetry;
-let selectColors;
+let canvas;
+let countrySelect
+let motifSelect
+let symmetrySelect
+let colorsSelect
 let exportButton;
-
-// TODO proper motif with corresponding scales
-let rScale = d3.scaleSqrt();
-let r;
-
-// define postcard size (A5 postcard: 210mm x 148mm)
-// calculation: https://imageonline.co/mm-to-px.php
-// laptop screen: 1920 x 1200, 14'' diagonal --> 162 ppi; 60Hz
-let pcWidthMM = 210; // mm
-let pcHeightMM = 148; // mm
-let dpi = 150; // dots per inch
-let pcWidth = Math.floor(pcWidthMM * dpi / 25.4);
-let pcHeight = Math.floor(pcHeightMM * dpi / 25.4);
-let pcPosX = 300; // x position of postcard
-let pcPosY = 100; // y position of postcard
-
-// define cell
-let cellWidth = 100;
-let cellHeight = 100;
-let nCells;
-
+        
 function setup() {
-  let cnv = createCanvas(pcWidth, pcHeight);
-  cnv.position(pcPosX, pcPosY);
-  cnv.style("width", 500 + "px") // scale down size of canvas with html code
-  noLoop();
-
-  // load data from file
-  // TODO continue here!
-  d3.csv("data/gccs_country_with_temperature_and_gdp.csv").then(data => {
-    console.log(data);
-    // TODO filter data by country
-
-  });
-
-
-  // create dropdown menu for country selection
-  // TODO change font size --> tailwindcss
-  selectCountry = createSelect();
-  selectCountry.position(30, 100);
-  selectCountry.option('Switzerland');
-  selectCountry.size(200, 50);
-
-  // create dropdown menu for motif selection
-  selectMotif = createSelect();
-  selectMotif.position(30, 160);
-  selectMotif.option('Motif 1');
-  selectMotif.size(200, 50);
-
-  // create dropdown menu for symmetry selection
-  selectSymmetry = createSelect();
-  selectSymmetry.position(30, 220);
-  selectSymmetry.option('Translation (3.1)');
-  selectSymmetry.size(200, 50);
-
-  // create dropdown menu for color selection
-  selectColors = createSelect();
-  selectColors.position(30, 280);
-  selectColors.option('Color Scheme 1');
-  selectColors.size(200, 50);
-
-  // create export button
-  // TODO export as other file formats (e.g. svg)
-  exportButton = createButton('Export postcard');
-  exportButton.position(30, 340);
-  exportButton.size(200, 50);
-  exportButton.mousePressed(() => {
-    saveCanvas('postcard', 'png');
-  });
-
-  // TODO test for cell sizes where multiplication with width and height does not result in integer values
-  nCells = Math.ceil(pcWidth / cellWidth) * Math.ceil(pcHeight / cellHeight);
-  console.log('nCells: ' + nCells);
-  console.log('pcWidth: ' + pcWidth + ', pcHeight: ' + pcHeight);
-
-
-  rScale.domain([0, 100]).range([1, Math.min(cellWidth, cellHeight) * 0.8]);
-
+    canvas = createCanvas(800, 600);
+    canvas.parent(document.querySelector('.canvas-container'));
+            
+    // Get references to HTML elements
+    countrySelect = document.getElementById('country');
+    motifSelect = document.getElementById('motif');
+    symmetrySelect = document.getElementById('symmetry');
+    colorsSelect = document.getElementById('colors');
+    exportButton = document.getElementById('exportButton');
+            
+    // Set initial background
+    //updateBackground();
+            
+    // Add event listeners
+    //backgroundSelect.addEventListener('change', updateBackground);
+    exportButton.addEventListener('click', exportCanvas);
+            
+    // Set initial drawing properties
+    strokeCap(ROUND);
+    strokeJoin(ROUND);
 }
-
+        
 function draw() {
-  background(220);
-  
-  // initialize
-  rectMode(TOP, LEFT);
-  let x = 0;
-  let y = 0;
-
-  // draw pattern
-  for (let i = 0; i < nCells; i++) {
-
-    // draw grid
-    drawGrid(x, y);
-
-    // draw motif
-    // TODO draw motif in separate for loop?
-    drawMotif(x, y);
-
-    // move to next cell: planar symmetry with translation
-    // TODO other symmetriy operations
-    x = x + cellWidth;
-    if (x >= pcWidth) {
-      x = 0;
-      y += cellHeight;
+    if (mouseIsPressed && mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        //// Get current settings
+        //let brushSize = parseInt(brushSizeSelect.value);
+        //let colorValues = brushColorSelect.value.split(',');
+        //let brushStyle = brushStyleSelect.value;
+                
+        //// Set color
+        //stroke(parseInt(colorValues[0]), parseInt(colorValues[1]), parseInt(colorValues[2]));
+        //strokeWeight(brushSize);
+                
+        //// Apply brush style
+        //if (brushStyle === 'dotted') {
+            //// Draw dots instead of lines
+            //noStroke();
+            //fill(parseInt(colorValues[0]), parseInt(colorValues[1]), parseInt(colorValues[2]));
+            //circle(mouseX, mouseY, brushSize);
+        //} else if (brushStyle === 'dashed') {
+            //// Draw dashed line effect
+            //if (frameCount % 3 === 0) {
+                //line(pmouseX, pmouseY, mouseX, mouseY);
+            //}
+        //} else if (brushStyle === 'rough') {
+            //// Add some randomness for rough effect
+            //for (let i = 0; i < 3; i++) {
+                //let offsetX = random(-brushSize/2, brushSize/2);
+                //let offsetY = random(-brushSize/2, brushSize/2);
+                //point(mouseX + offsetX, mouseY + offsetY);
+            //}
+        //} else {
+            //// Normal line
+            //line(pmouseX, pmouseY, mouseX, mouseY);
+        //}
     }
-
-  }
-
 }
-
-function drawGrid(x, y) {
-
-  // TODO dashed grid lines: https://github.com/processing/p5.js/issues/3336#issuecomment-441457612
-  noFill();
-  stroke(100);
-  strokeWeight(1);
-  rect(x, y, cellWidth, cellHeight);
-
+        
+function updateBackground() {
+    let bgValues = backgroundSelect.value.split(',');
+    background(parseInt(bgValues[0]), parseInt(bgValues[1]), parseInt(bgValues[2]));
 }
-
-function drawMotif(x, y) {
-  //console.log('drawMotif at x: ' + x + ', y: ' + y);
-  noStroke();
-
-  // wtp_1
-  r = rScale(gccs_values['wtp_1']);
-  fill(100, 150, 0);
-  arc(x + cellWidth/2, y + cellHeight/2, r, r, -PI/2, 0);
-
-  // wtp_1_belief
-  r = rScale(gccs_values['wtp_1_belief']);
-  fill(100, 0, 0);
-  arc(x + cellWidth/2, y + cellHeight/2, r, r, 0, PI/2);
-
-  // norm
-  r = rScale(gccs_values['norm']);
-  fill(0, 100, 150);
-  arc(x + cellWidth/2, y + cellHeight/2, r, r, PI/2, PI);
-
-  // government
-  r = rScale(gccs_values['government']);
-  fill(100, 0, 200);
-  arc(x + cellWidth/2, y + cellHeight/2, r, r, PI, -PI/2);
-
+        
+function exportCanvas() {
+    saveCanvas('my-drawing', 'png');
 }
-
-
-// use d3 to generate svg
-// p5 can only create raster images
-
-// with p5: very big canvas elements, which are then shown much smaller (performance!)
+        
+// Clear canvas when 'C' key is pressed
+function keyPressed() {
+    if (key === 'c' || key === 'C') {
+        updateBackground();
+    }
+}
